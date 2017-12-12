700d9f650fd7b5ea71e83326d3791932bf6c7e15
danielefumagalli <33418244+danielefumagalli@users.noreply.github.com>
2017-12-12 11:39:11 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -50,7 +50,7 @@
 			$json=curl_exec($ch) or die(curl_error());
 			# Decodifico json
 			$data = json_decode($json);
-			echo "<table align='center'class='flat-table'>";
+			echo "<table class='flat-table'>";
 			echo"<tbody>";
 				echo "<tr>";
 					echo "<th>NOME</th>";
